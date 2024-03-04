79088d78875edd99600240afff646d5dd3b5be92
Lesedi <skhosanalesedi888@gmail.com>
2024-03-04 10:06:53 +0200

    my Level3 individual project

---
+++ products.js
@@ -0,0 +1,56 @@
+const products = [
+    {
+      id: 0,
+      name: "Light Pink",
+      price: 129.99,
+      instock: 100,
+      description:
+        "",
+      imgSrc: "./img/light pink.jpeg",
+    },
+    {
+      id: 1,
+      name: "Hot Pink",
+      price: 129.99,
+      instock: 100,
+      description:
+        "",
+      imgSrc: "./img/hot pink.webp",
+    },
+    {
+      id: 2,
+      name: "Pulse Red",
+      price: 129.99,
+      instock: 100,
+      description:
+        "",
+      imgSrc: "./img/pulse red.jpeg",
+    },
+    {
+      id: 3,
+      name: "Shock Orange",
+      price: 129.99,
+      instock: 100,
+      description:
+        "",
+      imgSrc: "./img/shock orange.webp",
+    },
+    {
+      id: 4,
+      name: "Birthday Suit",
+      price: 129.99,
+      instock: 100,
+      description:
+        "",
+      imgSrc: "./img/nude.webp",
+    },
+    {
+      id: 5,
+      name: "Plum",
+      price: 129.99,
+      instock: 100,
+      description:
+        "",
+      imgSrc: "./img/plum.webp",
+    },
+  ];
